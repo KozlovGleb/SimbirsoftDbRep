faa8d2a32ba011d66516edf7bb5eb2ad7ba8220a
Gleb <gleb.samarskiy@mail.ru>
2020-12-11 16:36:03 +0400

    Добавил контроллер для докторов

--- Controllers/DoctorsController.cs
+++ Controllers/DoctorsController.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SimbirsoftDbRep.Common.Swagger;
 using SimbirsoftDbRep.Database.Context;
 using SimbirsoftDbRep.Database.Domain;
+using SimbirsoftDbRep.Models.DTO;
+using SimbirsoftDbRep.Models.Requests.Doctor;
+using SimbirsoftDbRep.Models.Responses.Doctor;
+using SimbirsoftDbRep.UoW;
 
 namespace SimbirsoftDbRep.Controllers
 {
@@ -16,97 +23,90 @@ namespace SimbirsoftDbRep.Controllers
     [ApiExplorerSettings(GroupName = SwaggerDocParts.Doctors)]
     public class DoctorsController : ControllerBase
     {
-        private readonly HospitalContext _context;
+        private readonly ILogger<PatientsController> _logger;
+        private readonly IMapper _mapper;
+        UnitOfWork unitOfWork;
 
-        public DoctorsController(HospitalContext context)
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="PatientsController"/>
+        /// </summary>
+        /// <param name="patientService">Сервис.</param>
+        /// <param name="logger">Логгер.</param>
+        /// <param name="mapper">Маппер.</param>
+        public DoctorsController( ILogger<PatientsController> logger, IMapper mapper, HospitalContext context)
         {
-            _context = context;
+            _logger = logger;
+            _mapper = mapper;
+            unitOfWork = new UnitOfWork(context, mapper);
         }
 
-        // GET: api/Doctors
+        /// <summary>
+        /// Получение перечня пациентов.
+        /// </summary>
+        /// <returns>Коллекция сущностей "Пациент".</returns>
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors()
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<DoctorResponce>))]
+        public async Task<IActionResult> GetAsync(CancellationToken cancellationToken)
         {
-            return await _context.Doctors.ToListAsync();
+            _logger.LogInformation("Patients/Get was requested.");
+            //var response = await _patientService.GetAsync(cancellationToken);
+            var response = await unitOfWork.Doctors.GetAsync(cancellationToken);
+            return Ok(_mapper.Map<IEnumerable<DoctorResponce>>(response));
         }
 
-        // GET: api/Doctors/5
+        /// <summary>
+        /// Получение пациента по Id.
+        /// </summary>
+        /// <returns>Cущность "Пациент".</returns>
         [HttpGet("{id}")]
-        public async Task<ActionResult<Doctor>> GetDoctor(long id)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DoctorResponce))]
+        public async Task<IActionResult> GetByIdAsync(long id, CancellationToken cancellationToken)
         {
-            var doctor = await _context.Doctors.FindAsync(id);
-
-            if (doctor == null)
-            {
-                return NotFound();
-            }
-
-            return doctor;
+            _logger.LogInformation("Patients/GetById was requested.");
+            //var response = await _patientService.GetAsync(id, cancellationToken);
+            var response = await unitOfWork.Doctors.GetAsync(id);
+            return Ok(_mapper.Map<DoctorResponce>(response));
         }
 
-        // PUT: api/Doctors/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutDoctor(long id, Doctor doctor)
-        {
-            if (id != doctor.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(doctor).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DoctorExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Doctors
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        /// <summary>
+        /// Добавление сущности "Пациент".
+        /// </summary>
+        /// <returns>Cущность "Пациент".</returns>
         [HttpPost]
-        public async Task<ActionResult<Doctor>> PostDoctor(Doctor doctor)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DoctorResponce))]
+        public async Task<IActionResult> PostAsync(CreateDoctorRequest request, CancellationToken cancellationToken)
         {
-            _context.Doctors.Add(doctor);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetDoctor", new { id = doctor.Id }, doctor);
+            _logger.LogInformation("Patients/Post was requested.");
+            // var response = await _patientService.CreateAsync(_mapper.Map<PatientDTO>(request));
+            var response = await unitOfWork.Doctors.CreateAsync(_mapper.Map<DoctorDTO>(request));
+            return Ok(_mapper.Map<DoctorResponce>(response));
         }
 
-        // DELETE: api/Doctors/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Doctor>> DeleteDoctor(long id)
+        /// <summary>
+        /// Изменение сущности "Пациент".
+        /// </summary>
+        /// <returns>Cущность "Пациент".</returns>
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DoctorResponce))]
+        public async Task<IActionResult> PutAsync(UpdateDoctorRequest request, CancellationToken cancellationToken)
         {
-            var doctor = await _context.Doctors.FindAsync(id);
-            if (doctor == null)
-            {
-                return NotFound();
-            }
-
-            _context.Doctors.Remove(doctor);
-            await _context.SaveChangesAsync();
-
-            return doctor;
+            _logger.LogInformation("Patients/Put was requested.");
+            //var response = await _patientService.UpdateAsync(_mapper.Map<PatientDTO>(request));
+            var response = await unitOfWork.Doctors.UpdateAsync(_mapper.Map<DoctorDTO>(request));
+            return Ok(_mapper.Map<DoctorResponce>(response));
         }
 
-        private bool DoctorExists(long id)
+        /// <summary>
+        /// Удаление сущностей "Пациент".
+        /// </summary>
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteAsync(CancellationToken cancellationToken, params long[] ids)
         {
-            return _context.Doctors.Any(e => e.Id == id);
+            _logger.LogInformation("Patients/Delete was requested.");
+            //await _patientService.DeleteAsync(ids);
+            await unitOfWork.Patients.DeleteAsync(ids);
+            return NoContent();
         }
     }
 }
---
+++ Models/Requests/Doctor/UpdateDoctorRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimbirsoftDbRep.Models.Requests.Doctor
+{
+    public class UpdateDoctorRequest
+    {
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
+        [Required]
+        public long Id { get; set; }
+    }
+}
